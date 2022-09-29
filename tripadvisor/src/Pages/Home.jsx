59ae4179824b64984c465106c05f967d8c874d5c
rohit0985 <rohitsharma4681@gmail.com>
2022-09-30 02:31:39 +0530

    added: top destination

--- tripadvisor/src/Pages/Home.jsx
+++ tripadvisor/src/Pages/Home.jsx
@@ -4,9 +4,13 @@ import SqrCard from "../Components/Cards/SqrCard"
 import RectCard from "../Components/Cards/RectCard"
 import HotelCard from "../Components/Cards/HotelCard"
 import MoreToExplore from "../Components/MoreToExplore/MoreToExplore"
+import TopDestinations from "../Components/TopDestinations/TopDestinations"
 
 const Home = () => {
     return (<>
+    <TopDestinations/>
+    <br />
+        <br />
     <MoreToExplore/>
     <br />
         <br />
